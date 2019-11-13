33d94772396da8b561e74eab75489007cbe2e449
seong min Kim <skattr13@naver.com>
2019-11-13 13:24:19 +0900

    Use ifstream

--- 객체실습/fstreamTest/fstreamTest/main.cpp
+++ 객체실습/fstreamTest/fstreamTest/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include <fstream>
 #include <cstdlib>
 
 using namespace std;
 
-int main() {
+/*int main() {
 	ofstream outClientFile{ "clients.txt", ios::out };
 
 	if (!outClientFile) {
@@ -24,4 +25,29 @@ int main() {
 		outClientFile << account << ' ' << name << ' ' << balance << endl;
 		cout << "? ";
 	}
-} //ofstream 颇老 积己
+}*/ //ofstream 颇老 积己
+
+void outputLine(int, const string&, double);
+
+int main() {
+	ifstream inClientFile("clients.txt", ios::in);
+
+	if (!inClientFile) {
+		cerr << "File could not be opened" << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	cout << left << setw(10) << "Account" << setw(13) << "Name" << "Balance\n" << fixed << showpoint;
+
+	int account;
+	string name;
+	double balance;
+
+	while (inClientFile >> account >> name >> balance) {
+		outputLine(account, name, balance);
+	}
+}
+
+void outputLine(int account, const string& name, double balance) {
+	cout << left << setw(10) << account << setw(13) << name << setw(7) << setprecision(2) << right << balance << endl;
+}
