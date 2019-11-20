2dd2545890714b30a362d967fdfbcd78fc1be590
seong min Kim <skattr13@naver.com>
2019-11-20 13:57:09 +0900

    Implement write credit.dat file code

--- 객체실습/Client/Client/main.cpp
+++ 객체실습/Client/Client/main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int main() {
+/*int main() {
 	ofstream outCredit{ "credit.dat", ios::out | ios::binary };
 
 	if (!outCredit) {
@@ -17,5 +17,39 @@ int main() {
 
 	for (int i{ 0 }; i < 100; i++) {
 		outCredit.write(reinterpret_cast<const char*>(&blankClinet), sizeof(ClientData));
+	}
+}*/
+
+int main() {
+	fstream outCredit{ "credit.dat", ios::in | ios::out | ios::binary };
+
+	if (!outCredit) {
+		cerr << "File could not be opened." << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	cout << "Enter account number (1 to 100, 0 to end input)\n? ";
+
+	int accountNumber;
+	string lastName;
+	string firstName;
+	double balance;
+
+	cin >> accountNumber;
+
+	while(accountNumber > 0 && accountNumber <= 100) {
+		cout << "Enter lastname, firstname and balance\n? ";
+		cin >> lastName;
+		cin >> firstName;
+		cin >> balance;
+
+		ClientData client{ accountNumber, lastName, firstName, balance };
+
+		outCredit.seekp((client.getAccountNumber() - 1) * sizeof(ClientData));
+
+		outCredit.write(reinterpret_cast<char*>(&client), sizeof(ClientData));
+
+		cout << "Enter account number\n? ";
+		cin >> accountNumber;
 	}
 }
